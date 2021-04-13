276588fe28cb3739a63e6821e643a008f5c600e0
Matheus Henrique <matherique@gmail.com>
2021-04-13 20:50:25 -0300

    starting add multiple photos using go rotines

--- instagram.go
+++ instagram.go
@@ -1,58 +1,83 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"regexp"
 	"strings"
-	"sync"
 )
 
-var mutex sync.Mutex
+func getPhoto(link string) string {
 
-func main() {
-	if len(os.Args) == 1 {
-		fmt.Println("no instagram link")
-		return
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", link, nil)
+
+	if err != nil {
+		panic(err)
 	}
 
-	LINK := string(os.Args[1])
-	client := &http.Client{}
+	req.Header.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
 
-	var wg sync.WaitGroup
-	wg.Add(1)
+	resp, err := client.Do(req)
 
-	go func() {
-		mutex.Lock()
-		req, err := http.NewRequest("GET", LINK, nil)
+	if err != nil {
+		panic(err)
+	}
 
-		if err != nil {
-			panic(err)
+	data, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	reg := regexp.MustCompile("<meta property=\"og:image\"([\\w\\W]+?)/>")
+	matches := reg.Find(data)
+
+	fmt.Println(string(data))
+
+	url := strings.Replace(string(matches), "<meta property=\"og:image\" content=\"", "", -1)
+	url = strings.Replace(url, "\" />", "", -1)
+
+	return fmt.Sprintf("AAAAAAAA%s", url)
+}
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() == 1 {
+		fmt.Fprintln(os.Stderr, "missing instagram link")
+		os.Exit(1)
+	}
+
+	/*
+		photos := make(chan string, flag.NArg())
+		links := make(chan string, flag.NArg())
+
+		go func() {
+			for l := range links {
+				fmt.Println(l)
+				photos <- getPhoto(l)
+			}
+		}()
+
+		go func() {
+			for l := range links {
+				fmt.Println(l)
+				photos <- getPhoto(l)
+			}
+		}()
+
+		for _, l := range flag.Args() {
+			links <- l
 		}
 
-		req.Header.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
+		close(links)
 
-		resp, err := client.Do(req)
-
-		if err != nil {
-			panic(err)
+		for i := 0; i < flag.NArg(); i++ {
+			fmt.Fprintln(os.Stdout, <-photos)
 		}
+	*/
 
-		data, _ := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
+	fmt.Println(getPhoto(flag.Arg(1)))
 
-		reg := regexp.MustCompile("<meta property=\"og:image\"([\\w\\W]+?)/>")
-		matches := reg.Find(data)
-
-		url := strings.Replace(string(matches), "<meta property=\"og:image\" content=\"", "", -1)
-		url = strings.Replace(url, "\" />", "", -1)
-
-		fmt.Fprintf(os.Stdout, fmt.Sprintf("%s\n", url))
-		mutex.Unlock()
-		wg.Done()
-	}()
-
-	wg.Wait()
 }
